e9c947b63f51b567ef43b9ff20962f9304c1a46d
Ingu Kang <igxactly@gmail.com>
2016-07-04 14:50:06 +0900

    WIP 1

---
+++ lg2bt.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	//	"bufio"
+	//	"encoding/binary"
+	"fmt"
+	//	"io"
+	//	"os"
+)
+
+func main() {
+	fmt.Println("Hello")
+	/*
+	   f_in, err := os.Open(os.Args[1])
+	   if err != nil {
+	       fmt.Println(err)
+	       os.Exit(1)
+	   }
+	   defer f_in.Close()
+
+	   reader := bufio.NewReader(f_in)
+
+	   f_out, err := os.OpenFile(os.Args[2], os.O_RDWR|os.O_CREATE, 0644)
+	   if err != nil {
+	       fmt.Println(err)
+	       os.Exit(1)
+	   }
+	   defer f_out.Close()
+	   writer := bufio.NewWriter(f_out)
+
+	   _, err = writer.Write(j)
+	   writer.Flush()
+	*/
+}
